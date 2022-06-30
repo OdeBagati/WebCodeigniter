5c1c5857d67473c979c5c7f2554690c34e7d8ea3
odebagati <bagatikusuma@gmail.com>
2022-06-30 22:12:09 +0800

    fix error Model Galeri

--- app/Models/Mdlgallery.php
+++ app/Models/Mdlgallery.php
@@ -44,7 +44,7 @@ class Mdlgallery extends Model
         else
         {
             $this->builder->insert($arrSave);
-            return $this->db->insertID();
+            return $this->builder->insert($arrSave);
         }
     }
 
