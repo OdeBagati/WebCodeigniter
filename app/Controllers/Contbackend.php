ed0b2e79ed144b1ab45840bc0d1380ee15002d00
odebagati <bagatikusuma@gmail.com>
2022-07-16 20:29:36 +0800

    add foto

--- app/Controllers/Contbackend.php
+++ app/Controllers/Contbackend.php
@@ -9,4 +9,22 @@ class Contbackend extends BaseController
 
         return view('back_end',$data);
     }
+
+    function sendMail()
+    {
+        $msg = 'Jasa joki TA only 2Jt';
+        $email = \Config\Services::email();
+        $config['mailType'] = 'html';
+        $email->initialize($config);
+
+        $email->setFrom('no-reply@jokita.com', 'Joki TA');
+        $email->setTo('putuaryawidiastawa@gmail.com');
+        //$email->setCC('another@another-example.com');
+        //$email->setBCC('them@their-example.com');
+
+        $email->setSubject('Joki TA');
+        $email->setMessage($msg);
+
+        $email->send();
+    }
 }
